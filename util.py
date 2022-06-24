1065bbb84b31dbeee2a9c42c707afcc73a736b02
myNameArnav <arnjain3@gmail.com>
2022-06-24 06:38:32 +0530

    formatted with black and fixed bug

--- util.py
+++ util.py
@@ -1,12 +1,23 @@
 def numMonth2strMonth(strMonth):
     m2m = {
-        "Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6,
-        "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12
+        "Jan": 1,
+        "Feb": 2,
+        "Mar": 3,
+        "Apr": 4,
+        "May": 5,
+        "Jun": 6,
+        "Jul": 7,
+        "Aug": 8,
+        "Sep": 9,
+        "Oct": 10,
+        "Nov": 11,
+        "Dec": 12,
     }
     return m2m[strMonth]
 
+
 def twelveTo24(hour, minute, amORpm):
-    if hour >= 12 & minute >= 60:
+    if hour <= 12 and minute <= 60:
         if amORpm == "AM":
             if hour == 12:
                 finalHour = 00
