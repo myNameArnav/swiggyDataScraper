c0c15e963f366a5837090538fb21fece4e641f06
myNameArnav <arnjain3@gmail.com>
2022-06-25 15:44:54 +0530

    confict merge

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from jsonMaker import jsonMaker
 
 if __name__ == "__main__":
     jsonMaker("arnavData", output=True)
-=======
-from jsonMaker import jsonMaker
-
-if __name__ == "__main__":
-    jsonMaker("arnavData", output=False)
->>>>>>> 20de975b4d399c97b3a36333be3899250ba1d546
